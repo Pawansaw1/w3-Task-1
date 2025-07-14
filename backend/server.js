1c29147f084c57d9469e05de563ac1eeab7fb91f
Pawansaw1 <pawankumarsahu620703@gmail.com>
2025-07-14 16:27:31 +0530

    change in port

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const app = express();
+const port = process.env.PORT || 5000 ;
 
 connectDB();
 
@@ -11,4 +12,4 @@ app.use(express.json());
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/claim', require('./routes/claimRoutes'));
 
-app.listen(5000, () => console.log('Server started on port 5000'));
+app.listen(port, () => console.log('Server started on port 5000'));
